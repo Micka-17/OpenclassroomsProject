fd24ccfe1a03eebb0a951a41535c94f920ef2f34
mickael boi <mickael.boi@sfr.fr>
2021-04-28 11:41:44 +0200

    correction

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const twoLink = document.getElementById("nav-bas-g");
 const firstLink = document.getElementById("activites");
 
 if (window.innerWidth < 520) {
-firstLink.onclick = function() {
-	this.style.color = '#0065FC';
-	twoLink.style.color = 'grey';
-	this.style.borderBottom = '2px solid blue';
-	twoLink.style.borderBottom = '2px solid grey';
-}
+	firstLink.onclick = function() {
+		this.style.color = '#0065FC';
+		twoLink.style.color = 'grey';
+		this.style.borderBottom = '2px solid blue';
+		twoLink.style.borderBottom = '2px solid grey';
+	}
 
-twoLink.onclick = function() {
-	this.style.color = '#0065FC';
-	firstLink.style.color = 'grey';
-	this.style.borderBottom = '2px solid blue';
-	firstLink.style.borderBottom = '2px solid grey';
-}
+	twoLink.onclick = function() {
+		this.style.color = '#0065FC';
+		firstLink.style.color = 'grey';
+		this.style.borderBottom = '2px solid blue';
+		firstLink.style.borderBottom = '2px solid grey';
+	}
 };
