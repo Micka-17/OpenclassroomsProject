518e23bb32c60b863cc9e0cad85db0e723870bbf
mickael boi <mickael.boi@sfr.fr>
2021-04-28 08:59:52 +0200

    maj 28/04

---
+++ script.js
@@ -0,0 +1,16 @@
+const twoLink = document.getElementById("nav-bas-g");
+const firstLink = document.getElementById("activites");
+
+firstLink.onclick = function() {
+	this.style.color = '#0065FC';
+	twoLink.style.color = 'grey';
+	this.style.borderBottom = '2px solid blue';
+	twoLink.style.borderBottom = '2px solid grey';
+}
+
+twoLink.onclick = function() {
+	this.style.color = '#0065FC';
+	firstLink.style.color = 'grey';
+	this.style.borderBottom = '2px solid blue';
+	firstLink.style.borderBottom = '2px solid grey';
+}
