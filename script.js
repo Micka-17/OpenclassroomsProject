c543c086b9d476b0a048e5aa246220ab287e867a
mickael boi <mickael.boi@sfr.fr>
2021-04-30 10:19:43 +0200

    rectif

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const twoLink = document.getElementById("nav-bas-g");
 const firstLink = document.getElementById("activites");
 
-if (window.innerWidth < 520) {
-	firstLink.onclick = function() {
-		this.style.color = '#0065FC';
-		twoLink.style.color = 'grey';
-		this.style.borderBottom = '2px solid blue';
-		twoLink.style.borderBottom = '2px solid grey';
-	}
 
-	twoLink.onclick = function() {
-		this.style.color = '#0065FC';
-		firstLink.style.color = 'grey';
-		this.style.borderBottom = '2px solid blue';
-		firstLink.style.borderBottom = '2px solid grey';
-	}
-};
+
+window.addEventListener('resize', function () { 
+    window.location.reload(); 
+		firstLink.onclick = function() {
+			if (window.innerWidth < 520) {
+				this.style.color = '#0065FC';
+				twoLink.style.color = 'grey';
+				this.style.borderBottom = '2px solid blue';
+				twoLink.style.borderBottom = '2px solid grey';
+			} else {
+				this.style.color = 'initial';
+				twoLink.style.color = 'initial';
+				this.style.borderBottom = 'none';
+				twoLink.style.borderBottom = 'none';
+				// todo remove color and bottom
+				// example borderBottom : none
+			}
+		}
+	
+		twoLink.onclick = function() {
+			if (window.innerWidth < 520) {
+				this.style.color = '#0065FC';
+				firstLink.style.color = 'grey';
+				this.style.borderBottom = '2px solid blue';
+				firstLink.style.borderBottom = '2px solid grey';
+			} else {
+				this.style.color = 'initial';
+				firstLink.style.color = 'initial';
+				this.style.borderBottom = 'none';
+				firstLink.style.borderBottom = 'none';
+				// todo remove color and bottom
+				// example borderBottom : none
+			}
+		}
+});
